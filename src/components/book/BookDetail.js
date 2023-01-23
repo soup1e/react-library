2bfd4f621677fb4d4b2c31072695dcf2f3467d72
Sam Adams <sam.adams7785@gmail.com>
2023-01-23 15:37:03 -0800

    Back to Catalog Button

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,6 +1,6 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 function BookDetail() {
   // const id = 1; // TODO: Use id from route
@@ -17,7 +17,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Link to="/books">To catalog...</Link>
+      <Book book={book} showDetail />
+    </>
+  );
 }
 
 export default BookDetail;
