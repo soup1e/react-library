892c8cb2b6aaf53b6bb4dc862ef66a0207c8edee
Sam Adams <sam.adams7785@gmail.com>
2023-01-23 15:19:09 -0800

    adding react router and homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path="/books">
+          <Link to="/">To Homepage...</Link>
+          <BookList />
+        </Route>
+
+        <Route path="/">
+          <h1>Welcome to Library</h1>
+          <Link to="/books">To Library...</Link>
+        </Route>
+      </Switch>
     </main>
   );
 }
